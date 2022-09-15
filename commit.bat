b0b9debf5f93155568fba6e6b4cff239cab416b0
Grant Dickerson <thmanintheshack@gmail.com>
2022-09-14 21:16:22 -0500

    test client

--- commit.bat
+++ commit.bat
@@ -1,9 +1,6 @@
 @ECHO OFF
 
-REM git remote add origin https://github.com/TheManInTheShack/LWOTC_GIJoe.git
-REM git branch -M main
-REM git push -u origin main
-
+git remote set-url origin https://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@github.com/TheManInTheShack/LWOTC_GIJoe.git
 
 git status
 
